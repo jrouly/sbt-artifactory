1021dc9c0c6a0ba64ea46342967115a6b4a09146
Michel Rouly <michel.rouly@gmail.com>
2021-03-31 10:11:36 -0400

    Expose artifactory publish resolver and credentials as keys (#6)

--- src/main/scala/net/rouly/sbt/artifactory/ArtifactoryPluginKeys.scala
+++ src/main/scala/net/rouly/sbt/artifactory/ArtifactoryPluginKeys.scala
@@ -1,17 +1,19 @@
 package net.rouly.sbt.artifactory
 
-import sbt.{Resolver, settingKey}
+import sbt._
 
 trait ArtifactoryPluginKeys {
 
   final val artifactory = settingKey[Artifactory]("Artifactory connection settings")
-
   final val artifactoryRealm = settingKey[String]("Artifactory credential realm")
+  final val artifactoryCredentials = settingKey[Credentials]("Artifactory connection credentials")
 
   final val artifactorySnapshotRepository = settingKey[String]("Artifactory snapshot repository")
   final val artifactoryReleaseRepository = settingKey[String]("Artifactory release repository")
 
   final val artifactorySnapshotResolver = settingKey[Resolver]("Artifactory snapshot resolver")
   final val artifactoryReleaseResolver = settingKey[Resolver]("Artifactory release resolver")
+
+  final val artifactoryResolver = settingKey[Resolver]("Artifactory publishTo resolver")
 
 }
